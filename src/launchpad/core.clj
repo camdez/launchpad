7cbeb3e2e126c76b9ebde4c476f798ad47395ab1
Cameron Desautels <camdez@gmail.com>
2023-02-05 16:52:03 -0600

    WIP 5

--- src/launchpad/core.clj
+++ src/launchpad/core.clj
@@ -15,7 +15,9 @@
   ;; Use `Audio MIDI Setup.app` to create a virtual MIDI device.  Open
   ;; 'IAC Driver' and enable it.
 
-  ;; Need to be in User 2 mode (top button 7) for sane addressing.
+  ;; Need to be in User 2 mode (top button 7) on the LaunchPad for
+  ;; sane addressing.  If the `init-board` doesn't look like a
+  ;; green "circle", then you're in the wrong mode.
 
 
   ;; Apple's Midi implementation is broken and doesn't handle
@@ -42,10 +44,11 @@
 (comment
   (def virtual-out (midi-out "IAC Driver Bus 1"))
   (def virtual-in (midi-in "IAC Driver Bus 1"))
+
   (midi/midi-handle-events virtual-in
-                      (fn [msg]
-                        ;;(clojure.pprint/pprint msg)
-                        ))
+                           (fn [msg]
+                             ;;(clojure.pprint/pprint msg)
+                             ))
 
   (def major-scale [1 0 1 0 1 1 0 1 0 1 0 1])
   (def minor-scale [1 0 1 0 1 0 1 1 0 1 0 1])
@@ -176,36 +179,39 @@
   ;(all-leds-on color-off)
   (midi-control lp-out 0 0))
 
-(comment
-  (note->coords 59)
-  (coords->note 4 5)
-  (all-on color-red)
-  (led-on 4 5 color-red)
-  (all-off))
-
-;;;;;
+;;;
 
 (def palette [color-off color-red-dim color-red color-amber-dim color-amber color-green-dim color-green])
 (def code->color
   (partial nth palette))
 
-(def board
-  [[1 0 0 0 0 0 0 1]
-   [1 0 0 1 1 0 0 0]
-   [0 0 0 0 0 0 0 0]
-   [0 0 0 1 1 0 0 0]
-   [0 0 0 0 1 0 0 0]
-   [0 0 0 0 0 0 0 0]
-   [0 0 0 0 0 0 0 0]
-   [0 0 0 0 0 0 0 0]])
-
-(def new-board
+(def empty-board
   [[0 0 0 0 0 0 0 0]
    [0 0 0 0 0 0 0 0]
    [0 0 0 0 0 0 0 0]
    [0 0 0 0 0 0 0 0]
    [0 0 0 0 0 0 0 0]
    [0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0]])
+
+(def init-board
+  [[0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0]
+   [0 0 0 6 6 0 0 0]
+   [0 0 6 6 6 6 0 0]
+   [0 0 6 6 6 6 0 0]
+   [0 0 0 6 6 0 0 0]
+   [0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0]])
+
+(def exit-board
+  [[0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0]
+   [0 0 0 2 2 0 0 0]
+   [0 0 2 2 2 2 0 0]
+   [0 0 2 2 2 2 0 0]
+   [0 0 0 2 2 0 0 0]
    [0 0 0 0 0 0 0 0]
    [0 0 0 0 0 0 0 0]])
 
@@ -223,8 +229,18 @@
      (take 8 $)
      (vec $))])
 
+(def bottle-board
+  [[0 0 0 0 0 0 0 0]
+   [0 0 2 2 2 2 0 0]
+   [0 0 0 2 2 0 0 0]
+   [0 0 2 1 1 2 0 0]
+   [0 0 2 1 1 2 0 0]
+   [0 0 2 1 1 2 0 0]
+   [0 0 2 1 1 2 0 0]
+   [0 0 0 2 2 0 0 0]])
+
 ;; TODO: it's possible to set entire rows at once, or even all LEDs!
-(defn draw-board [board]
+(defn draw-board! [board]
   (dotimes [row-idx (count board)]
     (let [row (nth board row-idx)]
       (dotimes [col-idx (count row)]
@@ -242,7 +258,7 @@
                          (and (on-board? x' y')
                               (pos? (get-in old-board [y' x'])))))
                count))]
-  (defn life-step [board]
+  (defn life-bstep [board]
     (forv [y (range 8)]
       (forv [x (range 8)]
         (let [live? (pos? (get-in board [y x]))
@@ -253,7 +269,7 @@
             (and (not live?) (=  3 n-cnt)) 6
             :else                          0))))))
 
-(defn gravity-step [board]
+(defn gravity-bstep [board]
   (loop [b board
          x 0
          y 0]
@@ -270,29 +286,66 @@
                b)
              (inc x) y))))
 
-(defn fade-step
+(defn fade-bstep
   ([board]
-   (fade-step board 2))
+   (fade-bstep board 2))
   ([board chance]
-   (vec (map #(vec (map (fn [el]
-                          (cond
-                            (zero? el)                el
-                            (zero? (rand-int chance)) (dec el)
-                            :else                     el))
-                        %))
-             board))))
+   (mapv #(mapv (fn [el]
+                  (cond
+                    (zero? el)                el
+                    (zero? (rand-int chance)) (dec el)
+                    :else                     el))
+                %)
+        board)))
 
-(defn rain-step
-  ([board] (rain-step board (/ 1 10)))
+(defn rain-bstep
+  ([board] (rain-bstep board (/ 1 10)))
   ([board prob]
      (update-in board [0]
                 (fn [row]
                   (mapv (fn [el]
-                          (if (< (rand) prob)
+                          (if (and (zero? el) (< (rand) prob))
                             (dec (count palette))
                             el))
                         row)))))
 
+(defn eq-step [{:keys [peaks] :as state}]
+  (let [levels (->> (fn low-bias-rand []
+                      (let [x (rand-int 5)]
+                        (if (< x 4)
+                          x
+                          (+ x (rand-int 5)))))
+                    repeatedly
+                    (take 8)
+                    vec)
+        board  (forv [y (range 8)]
+                 (forv [x (range 8)]
+                   (let [level     (nth levels x)
+                         logical-y (- 8 y)]
+                     (cond
+                       (>= logical-y level) 0
+                       (>  logical-y 5)     2 ; red
+                       (>  logical-y 2)     6 ; green
+                       :else                5 ; dim green
+                       ))))
+        peaks  (map (fn [level peak]
+                      (max level peak))
+                    levels
+                    (if peaks
+                      (map dec peaks)
+                      levels))
+        board  (vec
+                (reduce (fn [b [x peak]]
+                          (let [logical-y (- 8 peak)]
+                            (if (pos? peak)
+                              (assoc-in b [logical-y x] 4)
+                              b)))
+                        board
+                        (map-indexed vector peaks)))]
+    (assoc state
+           :eq-levels levels
+           :board     board
+           :peaks     peaks)))
 ;;;
 
 ;; Interestingly, it sounds like the device can natively draw text.
@@ -354,86 +407,62 @@
                 0
                 color)))
 
+(defn lift-bstep
+  "Convert a board step function to a step function.
+
+  (board -> board) -> (state -> state)."
+  [bstep-fn]
+  (fn [state]
+    (update state :board bstep-fn)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Scenes
 
 (def modes
-  {:paint   {:on-tap   paint
-             :on-reset (fn [{:keys [board old-board] :as state}]
-                         (if (and old-board (= board new-board)) ; undo
-                           (assoc state
-                                  :board old-board)
-                           (assoc state ; clear
-                                  :old-board board
-                                  :board new-board)))}
-   :gravity {:on-tap  paint
-             :on-tick #(update % :board gravity-step)}
-   :rain    {:on-tick (let [step (comp gravity-step fade-step rain-step)]
-                        #(update % :board step))}
-   :life    {:on-tap  paint
-             :on-tick #(update % :board life-step)}
-   :eq      {:enter   #(assoc % :tick-ms 600)
-             :on-tick (fn [{:keys [peaks] :as state}]
-                        (let [levels (->> (fn low-bias-rand []
-                                            (let [x (rand-int 5)]
-                                              (if (< x 4)
-                                                x
-                                                (+ x (rand-int 5)))))
-                                          repeatedly
-                                          (take 8)
-                                          vec)
-                              board  (for [y (range 8)]
-                                       (vec
-                                        (for [x (range 8)]
-                                          (let [level     (nth levels x)
-                                                logical-y (- 8 y)]
-                                            (cond
-                                              (>= logical-y level) 0
-                                              (>  logical-y 5)     2 ; red
-                                              (>  logical-y 2)     6 ; green
-                                              :else                5 ; dim green
-                                              )))))
-                              peaks  (map (fn [level peak]
-                                            (max level peak))
-                                          levels
-                                          (if peaks
-                                            (map dec peaks)
-                                            levels))
-                              board  (vec
-                                      (reduce (fn [b [x peak]]
-                                                (let [logical-y (- 8 peak)]
-                                                  (if (pos? peak)
-                                                    (assoc-in b [logical-y x] 4)
-                                                    b)))
-                                              board
-                                              (map-indexed vector peaks)))]
+  {:paint   {:on-tap  paint
+             :on-self (fn [{:keys [board old-board] :as state}]
+                        (if (and old-board (= board empty-board)) ; undo
                           (assoc state
-                                 :eq-levels levels
-                                 :board     board
-                                 :peaks     peaks)))}
-   :options {:enter   (fn [state]
-                        (assoc state
-                               :restore (select-keys state [:board :tick-ms])
-                               :board options-board
-                               :tick 0
-                               :tick-ms 500))
-             :leave   (fn [{:keys [restore] :as state}]
-                        (-> state
-                            (merge restore)
-                            (dissoc :restore)))
-             :on-tap  (fn [{:keys [board] :as state} x y]
-                        (when (= 7 y) ; color picker row
-                          (assoc state :color (get-in board [y x]))))
-             :on-tick (fn [{:keys [color tick] :as state}]
-                        (-> state
-                            (update :tick inc)
-                            (assoc :board
-                                   (cond-> options-board
-                                     (odd? tick)
-                                     (assoc-in [7 color] 0)))))}
-   :exit    {:enter (fn [{:keys [exit?] :as state}]
-                      (deliver exit? true)
-                      state)}})
+                                 :board old-board)
+                          (assoc state  ; clear
+                                 :old-board board
+                                 :board empty-board)))}
+
+   :gravity {:on-tap  paint
+             :on-tick (lift-bstep gravity-bstep)}
+
+   :rain    {:on-tick (lift-bstep (comp rain-bstep gravity-bstep fade-bstep))}
+
+   :life    {:on-tap  paint
+             :on-tick (lift-bstep life-bstep)}
+
+   :eq      {:on-enter #(assoc % :tick-ms 600)
+             :on-tick  eq-step}
+
+   :options {:on-enter (fn [state]
+                         (assoc state
+                                :restore (select-keys state [:board :tick-ms])
+                                :board options-board
+                                :tick 0
+                                :tick-ms 500))
+             :on-leave (fn [{:keys [restore] :as state}]
+                         (-> state
+                             (merge restore)
+                             (dissoc :restore)))
+             :on-tap   (fn [{:keys [board] :as state} x y]
+                         (when (= 7 y)  ; color picker row
+                           (assoc state :color (get-in board [y x]))))
+             :on-tick  (fn [{:keys [color tick] :as state}]
+                         (-> state
+                             (update :tick inc)
+                             (assoc :board
+                                    (cond-> options-board
+                                      (odd? tick)
+                                      (assoc-in [7 color] 0)))))}
+
+   :exit    {:on-enter (fn [{:keys [exit?] :as state}]
+                         (deliver exit? true)
+                         state)}})
 
 (def mode-buttons
   [:paint
@@ -445,36 +474,7 @@
    :options
    :exit])
 
-(defn initial-state []
-  {:board   new-board
-   :color   2
-   :tick-ms 200})
-
 ;;
-
-;; Tetris option?  This should be the `on-reset` on gravity mode!
-
-;; TODO: highlight selected mode (and rename them "scenes"!)
-
-;; TODO: we should probably pass `out` also.
-;; TODO: could support down and up for taps
-;; TODO: binary clock timer?  Or just block timer?  Hourglass? :)
-
-;; TODO: I could globally track all keys held down.  That would be
-;; neat.  Could also track the tick they went down and use that for
-;; long presses.
-
-;; TODO: "screenshot", kind of like a register.
-
-;; TODO: could do a repeat thing with `:init` where it loads some
-;; built-in "images".
-
-;; TODO: debug should be an option
-
-;; TODO: what about "colors" that rotate??
-
-;; All handlers should be allowed to return not maps which are
-;; ignored.
 
 (defn handle-msg [state-atom {:keys [command note] :as msg}]
   (prn (dissoc msg :msg :device))       ; DEBUG: messages
@@ -484,28 +484,29 @@
       (#{8 24 40 56 72 88 104 120} note)
       (when-let [new-mode (let [idx (int (/ (- note 8) 16))]
                             (nth mode-buttons idx))]
-        (let [{:keys [enter on-reset]} (get modes new-mode)
-              {:keys [mode]}           @state-atom
-              {:keys [leave]}          (get modes mode)]
+        (let [{:keys [on-enter on-self]} (get modes new-mode)
+              {:keys [mode]}             @state-atom
+              {:keys [on-leave]}         (get modes mode)
+              mode-changed?              (not= new-mode mode)]
           ;; TODO: Light up mode buttons
           (swap! state-atom
                  (fn select-mode [s]
                    (cond-> s
-                     (and leave (not= new-mode mode))
-                     leave
+                     (and on-leave mode-changed?)
+                     on-leave
 
                      true
-                     (assoc :mode new-mode)
+                     (assoc :mode new-mode) ; TODO: not needed if no mode change!
 
-                     (and enter (not= new-mode mode))
-                     enter
+                     (and on-enter mode-changed?)
+                     on-enter
 
-                     (and on-reset (= new-mode mode))
-                     on-reset)))))
+                     (and on-self (not mode-changed?))
+                     on-self)))))
 
       ;; Main grid
       (<= 0 note 119)
-      (let [{:keys [mode]}   @state-atom
+      (let [{:keys [mode]}   @state-atom ; might not want to deref here, could do it all in the `swap!`
             {:keys [on-tap]} (get modes mode)]
         (when on-tap
           (let [[x y] (note->coords note)]
@@ -516,44 +517,42 @@
                          ret
                          s))))))))))
 
-;; TODO: initial draw
-
 (defn run [in]
-  (let [mode  (first mode-buttons)
-        exit? (promise)
-        enter (or (get-in modes [mode :enter])
-                  identity)
-        state (-> (initial-state)
-                  (assoc :mode mode
-                         :exit? exit?)
-                  enter
-                  atom)
-        _     (add-watch state :draw
+  (let [exit? (promise)
+        state (atom nil)
+        _     (add-watch state :redraw
                          (fn [_k _r old new]
                            (when (not= (dissoc old :tick) (dissoc new :tick))
-                             (prn new)  ; DEBUG: state
+                             (prn new) ; DEBUG: state
                              (when (not= (:board old) (:board new))
                                ;; TODO: Optimize this to only send what changed
-                               (draw-board (:board new))))))
+                               (draw-board! (:board new))))))
+        _     (reset! state
+                      {:board   init-board
+                       :color   2
+                       :exit?   exit?
+                       :mode    (first mode-buttons)
+                       :tick-ms 200})
         tickf (future
-                (loop []
-                  (let [{:keys [mode tick-ms]} @state]
-                    (when-let [on-tick (get-in modes [mode :on-tick])]
-                      (swap! state on-tick))
-                    (Thread/sleep (or tick-ms 1000))
-                    (when-not (realized? exit?)
-                      (recur)))))]
+                (try ; TODO: if apply handlers checked for exceptions, this could be a lot cleaner!
+                  (loop []
+                    (let [{:keys [mode tick-ms]} @state]
+                      (when-let [on-tick (get-in modes [mode :on-tick])]
+                        (swap! state on-tick))
+                      (Thread/sleep (or tick-ms 1000))
+                      (when-not (realized? exit?)
+                        (recur))))
+                  (catch Exception ex
+                    (prn ex))))]
     (try
       (midi/midi-handle-events in (partial handle-msg state))
       @exit?                            ; wait for exit signal
+      (swap! state #(assoc % :board exit-board))
       (finally
         (.setReceiver (:transmitter in) nil)
         (println "Goodbye!")))
     @tickf                              ; check for exceptions
     @state))
-
-;; How about at option key?? That would be cool!  Could also have
-;; pages of tools.  Could even have a palette.
 
 (comment
   (def f (future (run lp-in)))
@@ -561,13 +560,6 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Unused
-
-;;; Next steps:
-;;; 1. Make a 2D vector representing the screen, add a function to draw it.
-;;; 2. Start running functions over the vector. Cellular automata. Blocks that fall. Etc.
-;;;    - Tap once for one color, tap again for another, etc.
-;;;    - Growing green plants, with a monster running around eating them.
-;;;    - Games
 
 (defn bits
   "Returns the lowest eight bits of number `n` as a vector."
@@ -600,13 +592,14 @@
     (Thread/sleep 1000)
     (recur)))
 
-
 (comment
   (doseq [y (range 8)
           x (range 8)]
     (led-on x y color-green)
     (Thread/sleep 20)
     (led-off x y)))
+
+;; This would make a nice, basic animation example.
 
 (defn wander-step
   ([i] (wander-step i 0 7))
